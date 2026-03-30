8c530bacc59b57b6de501a4a2a6fe999e2e48765
misuyama <144996653+misuyama@users.noreply.github.com>
2026-03-30 22:47:25 +0900

    Add files via upload

---
+++ 2026_03_29 Chart 2/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _2026_03_29_Chart_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.onepic = new System.Windows.Forms.Button();
+            this.twopic = new System.Windows.Forms.Button();
+            this.ch1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.ch1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // onepic
+            // 
+            this.onepic.Location = new System.Drawing.Point(214, 319);
+            this.onepic.Name = "onepic";
+            this.onepic.Size = new System.Drawing.Size(106, 23);
+            this.onepic.TabIndex = 0;
+            this.onepic.Text = "하나로 그리기";
+            this.onepic.UseVisualStyleBackColor = true;
+            this.onepic.Click += new System.EventHandler(this.onepic_Click);
+            // 
+            // twopic
+            // 
+            this.twopic.Location = new System.Drawing.Point(433, 319);
+            this.twopic.Name = "twopic";
+            this.twopic.Size = new System.Drawing.Size(117, 23);
+            this.twopic.TabIndex = 1;
+            this.twopic.Text = "쪼개서 그리기";
+            this.twopic.UseVisualStyleBackColor = true;
+            this.twopic.Click += new System.EventHandler(this.twopic_Click);
+            // 
+            // ch1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.ch1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.ch1.Legends.Add(legend3);
+            this.ch1.Location = new System.Drawing.Point(214, 12);
+            this.ch1.Name = "ch1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "영어";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "국어";
+            this.ch1.Series.Add(series5);
+            this.ch1.Series.Add(series6);
+            this.ch1.Size = new System.Drawing.Size(336, 300);
+            this.ch1.TabIndex = 2;
+            this.ch1.Text = "chart1";
+            this.ch1.Click += new System.EventHandler(this.ch1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ch1);
+            this.Controls.Add(this.twopic);
+            this.Controls.Add(this.onepic);
+            this.Name = "Form1";
+            this.Text = "차트";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ch1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button onepic;
+        private System.Windows.Forms.Button twopic;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ch1;
+    }
+}
+
---
+++ 2026_03_29 Chart 2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _2026_03_29_Chart_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ch1.Titles.Add("성적");
+
+            //ch1.Series.Add("series2"); //디자인 창에서 넣어도 되지만, 소스코드로 추가가 가능하다. 그것을 보여주려고 이렇게 한 것
+
+            //ch1.Series[0].LegendText = "수학"; //디자인 창에서도 추가 가능하다.
+            //ch1.Series[1].LegendText = "영어"; //디자인 창에서도 추가 가능하다.
+
+            Random r = new Random(); //Random 객체 r 생성
+
+            for (int i = 1; i < 10; i++)
+            {
+                ch1.Series[0].Points.AddXY(i, r.Next(101)); //수학점수 , addxy는 xy를 추가하라는 뜻이다. 즉 x에는 i가 들어가고, y에는 r이 들어간다. 
+                ch1.Series[1].Points.AddXY(i, r.Next(101)); //영어점수
+            }
+
+            onepic.Enabled = false;
+        }
+
+        private void ch1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void twopic_Click(object sender, EventArgs e)
+        {
+            if (ch1.ChartAreas.Count >= 2)
+            {
+                return;
+            }
+            ch1.ChartAreas.Add("ChartArea2");
+            ch1.Series[1].ChartArea = "ChartArea2";
+
+            twopic.Enabled = false;
+            onepic.Enabled = true;
+        }
+
+        private void onepic_Click(object sender, EventArgs e)
+        {
+
+            
+                if (ch1.ChartAreas.Count == 1)
+                {
+                    return;
+                }
+
+                ch1.ChartAreas.RemoveAt(1);
+                ch1.Series[1].ChartArea = "ChartArea1";
+
+                onepic.Enabled = false;   // 합치면 다시 못 누르게
+                twopic.Enabled = true;    // 다시 쪼갤 수 있게
+            }
+        }
+
+    }
+
