17531449338b53bd13f251cad744d79c7bf68f93
misuyama <144996653+misuyama@users.noreply.github.com>
2026-04-05 23:43:18 +0900

    Add files via upload

---
+++ 018_student class/2026_04_05 class/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2026_04_05_class
+{
+
+    class Hooing
+    {
+        private int x, y;
+
+        public Hooing (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int Getsum ()
+        { return x + y; }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hooing sum = new Hooing(5, 4);
+            Console.WriteLine("합은 {0} 이다.", sum.Getsum());
+        }
+    }
+}
