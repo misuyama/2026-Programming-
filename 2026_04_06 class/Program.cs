b5aaf46d54aa5ab8648d929ac2e10277bbc641ca
misuyama <144996653+misuyama@users.noreply.github.com>
2026-04-06 21:31:31 +0900

    Add files via upload

---
+++ 2026_04_06 class/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _2026_04_06_class
+{
+
+    class Rectangle
+    {
+        private int width, height;  //변수를 만들어줍니다. 항상 변수는 private로 해줍니다.
+        public Rectangle (int width, int height) //퍼블릭을 만들어줍니다.
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double GetArea()
+        {
+            return width * height;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (width + height);
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(10, 50);
+            Console.WriteLine("길이는 : {0}", r.GetArea());
+            Console.WriteLine("넓이는 : {0}",r.GetPerimeter());
+        }
+    }
+}
---
+++ 2026_04_06 class1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Xml.Linq;
+
+namespace _2026_04_06_class1
+{
+
+    class Student
+    {
+        private int age;
+        private string major;
+        private string name;
+        public Student (string name, int age , string major)
+        {
+            this.age = age;
+            this.major = major;
+            this.name = name;
+
+        }
+
+        public string Getname()
+        {
+            return name;
+        }
+
+        public int Getage()
+        {
+            return age;
+
+        }
+        public string Getmajor()
+        {
+            return major;
+
+        }
+
+        public void Displayinfo ()
+        {
+           
+            Console.WriteLine("이름 {0}, 나이 {1}, 전공 {2}", name, age, major);
+
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student it = new Student("김태우" , 23 ,"의료it");
+            it.Displayinfo();
+        }
+    }
+}
