a92496d0862e85041947ec5953ebe3ab45df0852
misuyama <144996653+misuyama@users.noreply.github.com>
2026-04-07 11:13:11 +0900

    Add files via upload

---
+++ 2026_04_07 test/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _2026_04_07_test
+{
+
+
+    class Circle
+    {
+        private double radius;
+        private string color;
+
+        public Circle (double radius, string color)
+        {
+            this.radius = radius;
+            this.color = color;
+        }
+
+      public double GetArea()
+      {
+          return 2 * 3.14 * radius;
+      }
+      
+      public string Printinfo()
+      {
+          return color;
+      }
+
+        public void a()
+        {
+            Console.WriteLine($"면적은 {GetArea():F1}입니다.");
+            Console.WriteLine("정보는" + Printinfo() + "입니다.");
+        }
+
+
+    }
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Circle Hooing = new Circle(10, "빨간색");
+           Console.WriteLine($"면적은 {Hooing.GetArea():F1}입니다.");
+           Console.WriteLine("정보는" + Hooing.Printinfo() + "입니다.");
+            Hooing.a();
+        }
+    }
+}
