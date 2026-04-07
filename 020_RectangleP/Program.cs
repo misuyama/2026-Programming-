3cf4b4106748a7ff495ea75bfe4f9375b318172b
misuyama <144996653+misuyama@users.noreply.github.com>
2026-04-07 11:46:47 +0900

    Add files via upload

---
+++ 020_RectangleP/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _020_RectangleP
+{
+
+    class Rectangle
+    {
+        // int width, height; // 필드
+        public int width { get; set; } // 속성(대문자) public
+        public int height { get; set; }
+
+        public int GetArea()
+        {
+            return width * height;
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle   r = new Rectangle();
+            r.width = 10;
+            r.height = 10;
+            r.GetArea();
+            Console.WriteLine("면적 = " + r.GetArea);
+
+        }
+    }
+}
