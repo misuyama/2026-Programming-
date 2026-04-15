f7d3541ec83cd6e71827d3e68e89e68be87d10ab
misuyama <144996653+misuyama@users.noreply.github.com>
2026-04-15 16:45:40 +0900

    Add files via upload

---
+++ 023_FirebaceCRUD/date.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _022_FirebaceCRUD
+{
+    internal class Data
+    {
+        public string  Id { get; set; } //함수는 아니야. 하지만 함수같은 몸을 가지고 있어, 즉 속성이야.  번호 속성
+        public string Name { get; set; }  // 이름
+        public string Phone { get; set; } // 전화 속성
+
+        public string SId { get; set; }  // 학번 속성
+
+
+    }
+}
